60403ce86157280f6ee75aa714e21b6ccb1f1d26
Моисеева Ульяна 15.ИСиП.20.ОФ.О.2 <116793028+HuligankaUlianka2003@users.noreply.github.com>
2022-12-10 14:16:17 +0300

    Create 2.bat

---
+++ 2.bat
@@ -0,0 +1,11 @@
+bash-3.2$ mkdir workspace
+bash-3.2$ cd workspace/
+bash-3.2$ whoami nikitakhavatov
+bash-3.2$ echo "hello"; echo "world"
+bash-3.$2 echo "hello" && echo "world"
+bash-3.$2 echo "hello" || echo "world"
+bash-3.$2 echa "hello"; echo "world"
+bash-3.$2 echa "hello" || echo "world"
+bash-3.$2 echo "hello" || echa "world"
+bash-3.$2 echa "hello" || echa "world"
+bash-3.$2 yes
